e12728d9623ff49fb319e4f064e04b9924232638
Stepan Haida <stepanhaida@gmail.com>
2023-08-30 15:46:26 +0300

    init comit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import PostsCarousel from "./components/PostsCarousel"
+
+function App() {
+  return (
+    <>
+     <PostsCarousel />
+    </>
+  )
+}
+
+export default App
---
+++ src/components/PostsCarousel.tsx
@@ -0,0 +1,11 @@
+import { useState } from 'react'
+
+const PostsCarousel = () => {
+  const [posts, setPosts] = useState([]);
+  
+  return (
+    <div>PostsCarousel</div>
+  )
+}
+
+export default PostsCarousel
